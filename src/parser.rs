5a3b65d85646c7480d136b459f9ac4b4d8919a75
Peter Heringer <heringer@protonmail.com>
2022-08-06 19:09:54 +0200

    Add file reader

--- src/parser.rs
+++ src/parser.rs
@@ -1,10 +1,16 @@
 use std::fs;
+use std::error::Error;
+
+use crate::sequence::Sequence;
 use crate::sequence::dna_sequence::{DNASequence, DNucleotide};
 
 pub fn read_dna(filename: &str) -> Result<Vec<DNASequence>, Box<dyn Error>> {
     let contents = fs::read_to_string(filename)?;
-    parse_dna(contents)
+    parse_dna(&contents)
 }
 
-pub fn parse_dna(text: &str) -> Result<Vec<DNASequence>, Box<dyn Error>> {
+pub fn parse_dna(text: &str) -> Result<Vec<DNASequence>, &str> {
+    let lines: Vec<_> = text.lines().collect();
+    let sequences: Result<Vec<_>, _> = lines.chunks(2).map(|x| DNASequence::create(x[1])).collect();
+    Ok(sequences?.iter().map(|x| *x).collect::<Vec<_>>())
 }
--- src/sequence.rs
+++ src/sequence.rs
@@ -2,7 +2,7 @@ use std::ops::Index;
 
 pub mod dna_sequence;
 
-trait Sequence: IntoIterator + Index<usize> + Eq {
+pub trait Sequence: IntoIterator + Index<usize> + Eq {
 
     fn create(seq: &str) -> Result<Box<Self>, &str>;
 
